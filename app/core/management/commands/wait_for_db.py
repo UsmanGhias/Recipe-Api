df3b11140cad88ea3852c95d7aa60bb1513bb877
Usman Ghias <bsef20m540@pucit.edu.pk>
2023-08-09 22:00:30 +0500

    Database Covered all

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,34 +1,17 @@
 from django.core.management.base import BaseCommand
 
-"""_summary_
-    
-    Django command to pause execution until database is available
-    
+"""
+_summary_
+Django command to pause execution until database is available
 """
 
-class command(BaseCommand):
-    """_summary_
-        
-        Django Command to wait for database
-        
+
+class Command(BaseCommand):
     """
+    _summary_
+    Django Command to wait for database
+    """
+
     def handle(self, *args, **options):
-        
+
         pass
-        
-        
-        # """_summary_
-            
-        #     Handle the command
-            
-        # """
-        # self.stdout.write('Waiting for database...')
-        # db_conn = None
-        # while not db_conn:
-        #     try:
-        #         db_conn = connections['default']
-        #     except OperationalError:
-        #         self.stdout.write('Database unavailable, waiting 1 second...')
-        #         time.sleep(1)
-        # self.stdout.write(self.style.SUCCESS('Database available!'))    
-    
